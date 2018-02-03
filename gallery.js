ff06ffcab15fe2ca8c3a885705a27c47e0512495
Josh Boros <josh.boros@gmail.com>
2018-02-03 11:53:32 -0600

    We need to have the gallery boxes generate with the populate(artworks) function.

--- gallery.js
+++ gallery.js
@@ -1,20 +1,26 @@
 
 function loadJsonDB(floor) {
-    var xhttp = new XMLHttpRequest();
+    let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            jsonObj = JSON.parse(xhttp.responseText);
-            populate(jsonObj);
+            populate(JSON.parse(xhttp.responseText));
         }
     };
     xhttp.open("GET", floor + ".json", true);
     xhttp.send();
 }
 function populate(artworks){
-    var out = "";
-    for (var i=0; i < artworks.length; i++){
-        out += artworks[i].name + "\n";
+    let out = "";
+    //We may want to populate the image boxes and images here too.
+    
+    for (let i = 0; i < artworks.length; i++){
+        out +="<br>";
+        out += "<em>" + artworks[i].name + "</em><br>" + artworks[i].subName + " by " + artworks[i].artist.firstName + " " + artworks[i].artist.middleName + " " + artworks[i].artist.lastName + "<br>";
     }
     document.getElementById("demo").innerHTML = out;
 }
-    
+$(document).click(function(e) {
+    if (!$(e.target).is('a.dropdown-toggle')) {
+        $('.collapse').collapse('hide');        
+    }
+});
