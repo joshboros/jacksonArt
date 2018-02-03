485154ed472feff284a7a721e169c8f598474551
Josh Boros <josh.boros@gmail.com>
2018-02-03 16:18:00 -0600

    Fixed artwork-artist pairs. Added fade-in animation for gallery. I switched the jquery library with the full version, rather than the bootstrap min version.

--- gallery.js
+++ gallery.js
@@ -10,16 +10,13 @@ function loadJsonDB(floor) {
     xhttp.send();
 }
 function populate(artworks){
-    let out = "";
+    $("#previewsHere").empty();
     //We may want to populate the image boxes and images here too.
-    
     for (let i = 0; i < artworks.length; i++){
         let thumbnailTag = "<em>" + artworks[i].name + "</em><br>" + artworks[i].subName + " by " + artworks[i].artist.firstName + " " + artworks[i].artist.middleName + " " + artworks[i].artist.lastName + "<br>";
-        //out +="<br>";
-        out +='<div class="col-sm-4"><div class="thumbnail"><a href="https://placeholder.com"><img src="http://via.placeholder.com/1920x1080" style="width:100%"></a><div id="thumbCaption" class="caption"><p>' + thumbnailTag + '</p></div></a></div></div>'
         
+        $("#previewsHere").append($('<div class="col-sm-4"><div class="thumbnail"><a href="https://placeholder.com"><img src="http://via.placeholder.com/1920x1080" style="width:100%"></a><div id="thumbCaption" class="caption"><p>' + thumbnailTag + '</p></div></a></div></div>').hide().fadeIn("3000"));
     }
-    document.getElementById("previewsHere").innerHTML = out;
 }
 $(document).click(function(e) {
     if (!$(e.target).is('a.dropdown-toggle')) {
