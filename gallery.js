4814caa144cb82e813b00b72f14f5ca03f1fe1f6
Josh Boros <josh.boros@gmail.com>
2018-02-04 15:20:20 -0600

    added comments

--- gallery.js
+++ gallery.js
@@ -15,9 +15,11 @@ function populate(artworks){
     for (let i = 0; i < artworks.length; i++){
         let thumbnailTag = "<em>" + artworks[i].name + "</em><br>" + artworks[i].subName + " by " + artworks[i].artist.firstName + " " + artworks[i].artist.middleName + " " + artworks[i].artist.lastName + "<br>";
         
+        
         $("#previewsHere").append($('<div class="col-sm-4"><div class="thumbnail"><a href="https://placeholder.com"><img src="http://via.placeholder.com/1920x1080" style="width:100%"></a><div class="caption"><p>' + thumbnailTag + '</p></div></a></div></div>').hide().fadeIn("3000"));
     }
 }
+//This function collapses the navbar on click.
 $(document).click(function(e) {
     if (!$(e.target).is('a.dropdown-toggle')) {
         $('.collapse').collapse('hide');        
