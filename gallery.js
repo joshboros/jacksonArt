a7ea028430c174845de5692b1400f61770e77d1f
Josh Boros <josh.boros@gmail.com>
2018-02-20 15:22:43 -0600

    carousel home page added to Gallery link

--- gallery.js
+++ gallery.js
@@ -10,18 +10,25 @@ function loadJsonDB(floor) {
     xhttp.send();
 }
 function populate(artworks){
+    $("#myCarousel").hide();
     $("#previewsHere").empty();
     //We may want to populate the image boxes and images here too.
+    
     for (let i = 0; i < artworks.length; i++){
         let thumbnailTag = "<em>" + artworks[i].name + "</em><br>" + artworks[i].subName + " by " + artworks[i].artist.firstName + " " + artworks[i].artist.middleName + " " + artworks[i].artist.lastName + "<br>";
         
         
-        $("#previewsHere").append($('<div class="col-sm-4"><div class="thumbnail"><a href="https://placeholder.com"><img src="http://via.placeholder.com/1920x1080" style="width:100%"></a><div class="caption"><p>' + thumbnailTag + '</p></div></a></div></div>').hide().fadeIn("3000"));
+        $("#previewsHere").append($('<div class="col-sm-4 border border-dark"><div class="thumbnail"><a href="https://placeholder.com"><img src="http://via.placeholder.com/1920x1080" style="width:100%; padding:.25rem;border:1px solid; border-radius:.25rem;"></a><div class="thumbnail"><p>' + thumbnailTag + '</p></div></a></div></div>').hide().fadeIn("3000"));
     }
 }
+function toGallery(){
+        $("#previewsHere").empty();
+        $("#myCarousel").show();
+    }
 //This function collapses the navbar on click.
 $(document).click(function(e) {
     if (!$(e.target).is('a.dropdown-toggle')) {
-        $('.collapse').collapse('hide');        
+        $('.collapse').collapse('hide');
     }
 });
+
