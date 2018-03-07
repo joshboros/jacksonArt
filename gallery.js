4e7b4f4391b3cc07686db5731b810ec9beff75a4
Josh Boros <josh.boros@gmail.com>
2018-03-07 16:07:05 -0600

    b

--- gallery.js
+++ gallery.js
@@ -1,23 +1,32 @@
-/*
-function loadJsonDB() {
-    strw = new String("");
-    for(let i = 0; i < artworks.length; i++){
-         strw = strw.concat(artworks[i].title + '\n');
+window.addEventListener("hashchange", hashEvent, false);
+function hashEvent() {
+    as = document.getElementsByClassName("menudest");
+    location.hash === as.item();
+    for (i of as){
+        i.hash;
     }
-    alert(strw);
 }
-*/
-function populate(artworks){
+
+function populate(){
     $("#myCarousel").hide();
     $("#previewsHere").empty();
     //We may want to populate the image boxes and images here too.
-    
+    as = document.getElementsByClassName("menudest");
+    //Populate by floor.
+    for (i of as){
+        console.log(i.href);
+        var newDiv = document.createElement("div");
+    }
     for (let i = 0; i < artworks.length; i++){
         var thumbnailTag = 
         "<em>" + artworks[i].name + "</em><br>" + artworks[i].subName + " by " + artworks[i].artist.firstName + " " + artworks[i].artist.middleName + " " + artworks[i].artist.lastName + "<br>";
         
         $("#previewsHere").append($('<div class="col-sm-4"><div class="thumbnail"><a href="https://placeholder.com"><picture><img src="http://via.placeholder.com/1920x1080" style="width:100%; padding:.25rem;"></a></picture><div class="thumbnail"><p>' + thumbnailTag + '</p></div></a></div></div>').hide().fadeIn("3000"));
     }
+}
+
+function generatePage(){
+    var fragment = document.createDocumentFragment();
 }
 function toGallery(){
         $("#previewsHere").empty().fadeIn("3000");
@@ -31,7 +40,7 @@ $(document).click(function(e) {
     }
 });
 
-$( document ).ready(function() {
+$( function () {
     console.log( "ready!" );
 });
 
