cb12f291eb1d2235dfd629be6fd17c83e680902d
Josh Boros <josh.boros@gmail.com>
2018-03-09 08:06:05 -0600

    better

--- gallery.js
+++ gallery.js
@@ -1,27 +1,48 @@
-window.addEventListener("hashchange", hashEvent, false);
+window.onhashchange = hashEvent;
 function hashEvent() {
-    as = document.getElementsByClassName("menudest");
-    location.hash === as.item();
-    for (i of as){
-        i.hash;
+    $('.floor').hide();
+    hashsplit = location.hash.split('-');
+    switch (hashsplit[0]){
+        case "#floor":
+            populate(hashsplit[1]);
+            break;
+        case "viewer":
+
+            break
+        default:
     }
 }
 
-function populate(){
+
+function viewer(artid){
+    $('#viewImage').modal();
+    $('#viewImage').on('shown.bs.modal', function(){
+        $('#viewImage .modal-body').html(html);
+        //new code
+        $('a.controls').trigger('click');
+    })
+    $('#myModal').on('hidden.bs.modal', function(){
+        $('#myModal .modal-body').html('');
+    });
+
+}
+
+function populate(floor){
     $("#myCarousel").hide();
     $("#previewsHere").empty();
+
     //We may want to populate the image boxes and images here too.
-    as = document.getElementsByClassName("menudest");
     //Populate by floor.
-    for (i of as){
-        console.log(i.href);
         var newDiv = document.createElement("div");
-    }
-    for (let i = 0; i < artworks.length; i++){
-        var thumbnailTag = 
-        "<em>" + artworks[i].name + "</em><br>" + artworks[i].subName + " by " + artworks[i].artist.firstName + " " + artworks[i].artist.middleName + " " + artworks[i].artist.lastName + "<br>";
-        
-        $("#previewsHere").append($('<div class="col-sm-4"><div class="thumbnail"><a href="https://placeholder.com"><picture><img src="http://via.placeholder.com/1920x1080" style="width:100%; padding:.25rem;"></a></picture><div class="thumbnail"><p>' + thumbnailTag + '</p></div></a></div></div>').hide().fadeIn("3000"));
+        newDiv.id = "floor-" + floor;
+        newDiv.className = "floor";
+    var artworksonfloor = artworks.filter(artwork => artwork.floor == floor);
+    for (let art of artworksonfloor){
+        var thumbnailTag =
+        "<em>" + art.title + "</em><br>" + art.subTitle + " by " + art.artist.firstName + " " + art.artist.middleName + " " + art.artist.lastName + "<br>";
+        let imgpath = 'images/' + art.filename;
+        let about = art.title + art.subTitle;
+        $("#previewsHere").append($('<div class="col-sm-4"><div class="thumbnail"><a class="image-popup-link" href="' + imgpath + '" title="' + about + '"-><picture><img src="' + imgpath + '" style="width:100%; padding:.25rem;"></a></picture><div class="thumbnail"><p>' + thumbnailTag + '</p></div></a></div></div>').hide().fadeIn("3000"));
     }
 }
 
@@ -40,7 +61,26 @@ $(document).click(function(e) {
     }
 });
 
-$( function () {
-    console.log( "ready!" );
+$(document).ready(function() {
+    $('.image-popup-link').magnificPopup({
+        type: 'image',
+        mainClass: 'mfp-with-zoom',
+        zoom: {
+            enabled: true, // By default it's false, so don't forget to enable it
+
+            duration: 300, // duration of the effect, in milliseconds
+            easing: 'ease-in-out', // CSS transition easing function
+
+            // The "opener" function should return the element from which popup will be zoomed in
+            // and to which popup will be scaled down
+            // By defailt it looks for an image tag:
+            // other options
+            opener: function(openerElement) {
+                // openerElement is the element on which popup was initialized, in this case its <a> tag
+                // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+                return openerElement.is('img') ? openerElement : openerElement.find('img');
+            }
+        }
+    });
 });
 
