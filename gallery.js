91336d11b3ba44ba7eead0478f813599d5f7d9c5
Josh Boros <josh.boros@gmail.com>
2018-03-09 10:42:36 -0600

    f

--- gallery.js
+++ gallery.js
@@ -1,15 +1,35 @@
-window.onhashchange = hashEvent;
-function hashEvent() {
-    $('.floor').hide();
+window.addEventListener("hashchange", hashEvent, false);
+function hashEvent(event) {
+    console.log('hashchange');
+    $('.pages').hide();
+
     hashsplit = location.hash.split('-');
     switch (hashsplit[0]){
-        case "#floor":
-            populate(hashsplit[1]);
-            break;
-        case "viewer":
+        case "#home":
+            home = document.getElementById('home-0');
+            $('home').show();
+            $("#myCarousel").show().fadeIn("3000");
+            $('#myCarousel').fadeIn();
 
-            break
+            break;
+        case "#floor":
+            if (!document.getElementById('floor-' + hashsplit[1])){
+                main = document.getElementsByTagName('main');
+                var floorel = document.createElement('div');
+                floorel.id = 'floor-' + hashsplit[1];
+                floorel.className = 'pages';
+                main[0].appendChild(floorel);
+                $('floor' + hashsplit[1]).show();
+                populate(hashsplit[1]);
+            } else {
+                $('floor' + hashsplit[1]).show();
+            }
+            break;
+        case "#viewer":
+
+            break;
         default:
+
     }
 }
 
@@ -29,7 +49,19 @@ function viewer(artid){
 
 function populate(floor){
     $("#myCarousel").hide();
-    $("#previewsHere").empty();
+    var floordiv = document.getElementById('floor-' + floor);
+
+    var galleryContainer = document.createElement('div');
+    galleryContainer.className = 'galleryContainer';
+    galleryContainer.style = "font-family: 'EB Garamond', serif;";
+
+
+    var previewsHere = document.createElement('div');
+    previewsHere.id = 'previewsHere' + floor;
+    previewsHere.className = 'previewsHere row img-fluid';
+
+    floordiv.appendChild(galleryContainer);
+    galleryContainer.appendChild(previewsHere);
 
     //We may want to populate the image boxes and images here too.
     //Populate by floor.
@@ -42,18 +74,14 @@ function populate(floor){
         "<em>" + art.title + "</em><br>" + art.subTitle + " by " + art.artist.firstName + " " + art.artist.middleName + " " + art.artist.lastName + "<br>";
         let imgpath = 'images/' + art.filename;
         let about = art.title + art.subTitle;
-        $("#previewsHere").append($('<div class="col-sm-4"><div class="thumbnail"><a class="image-popup-link" href="' + imgpath + '" title="' + about + '"-><picture><img src="' + imgpath + '" style="width:100%; padding:.25rem;"></a></picture><div class="thumbnail"><p>' + thumbnailTag + '</p></div></a></div></div>').hide().fadeIn("3000"));
+
+        $("#previewsHere" + floor).append($('<div class="col-sm-4"><div class="thumbnail"><a class="image-popup-link" href="' + imgpath + '" title="' + about + '"-><picture><img src="' + imgpath + '" style="width:100%; padding:.25rem;"></a></picture><div class="thumbnail"><p>' + thumbnailTag + '</p></div></a></div></div>').hide().fadeIn("3000"));
     }
 }
 
 function generatePage(){
     var fragment = document.createDocumentFragment();
 }
-function toGallery(){
-        $("#previewsHere").empty().fadeIn("3000");
-        $("#myCarousel").show().fadeIn("3000");
-        $('myCarousel').fadeIn()
-    }
 //This function collapses the navbar on click.
 $(document).click(function(e) {
     if (!$(e.target).is('a.dropdown-toggle')) {
@@ -62,12 +90,16 @@ $(document).click(function(e) {
 });
 
 $(document).ready(function() {
+    unique = [...new Set(artworks.map(art => art.floor )) ]
     $('.image-popup-link').magnificPopup({
         type: 'image',
         mainClass: 'mfp-with-zoom',
         zoom: {
             enabled: true, // By default it's false, so don't forget to enable it
-
+            closeOnContentClick: true,
+            image: {
+                verticalFit: false
+            },
             duration: 300, // duration of the effect, in milliseconds
             easing: 'ease-in-out', // CSS transition easing function
 
